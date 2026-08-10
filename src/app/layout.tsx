e30b637fedc0b8a1bee3836ebbf788a463392a70
Abdelrhman Abdelaal <aabdelaal@appswave.io>
2026-08-10 12:17:21 +0300

    style: update global styles and layout theme color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,6 +37,7 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
+  themeColor: "#36384d",
 };
 
 export default function RootLayout({
