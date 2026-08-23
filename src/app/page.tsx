44f57159eb72a2e505915ceeaa37a094c78e7f16
Abdelrhman Abdelaal <aabdelaal@appswave.io>
2026-08-23 15:46:12 +0300

    style: adjust padding for main container to improve layout responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import NewsletterCard from "@/components/NewsletterCard";
 
 export default function Home() {
   return (
-    <main className="flex flex-1 flex-col sm:grid sm:place-items-center sm:p-6 lg:p-10">
+    <main className="flex flex-1 flex-col max-sm:pb-6 sm:grid sm:place-items-center sm:p-6 lg:p-10">
       <NewsletterCard />
     </main>
   );
